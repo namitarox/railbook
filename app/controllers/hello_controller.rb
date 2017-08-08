0d42bd35254566db1c11452d7ad7fb3e68af1ef5
浪岡大貴 <namita0517@gmail.com>
2017-08-08 23:01:42 +0900

    コミット

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -2,5 +2,11 @@ class HelloController < ApplicationController
   def index
     render plain: 'こんにちは、世界！'
   end
+  def view
+    @msg = 'こんにちは、世界！'
+  end
+  def list
+    @books = Book.all
+  end
 
 end
